56f8c28abbdeca6c8cc8716060f41af67e8eb6e3
Harshit Badaya <harshitbadaya95@gmail.com>
2021-06-14 22:52:54 +0530

    commit meesge

--- index.js
+++ index.js
@@ -1,27 +1,12 @@
+const express = require("express");
+const app = express();
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+
+});
+app.listen(3000, function() {
+    console.log("Server is running on localhost3000");
+});
+
 
 // Write Javascript code!
-
-function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log(profile)
-    $("#name").text(profile.getName());
-    $("#email").text(profile.getEmail());
-    $("#image").attr('src', profile.getImageUrl());
-    $(".data").css("display", "block");
-    $(".g-signin2").css("display", "none");
-      console.log('ID: ' + profile.getId());
-  console.log('Full Name: ' + profile.getName());
-  console.log('Given Name: ' + profile.getGivenName());
-  console.log('Family Name: ' + profile.getFamilyName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail());
-}
-
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function() {
-        alert("You have been signed out successfully");
-        $(".data").css("display", "none");
-        $(".g-signin2").css("display", "block");
-    });
-}
